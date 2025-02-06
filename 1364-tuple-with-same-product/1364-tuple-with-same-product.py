ad0a7d72b4d137a1e6cc4db9871e1ec6408055c5
Ahmed Furkhan <108826853+Ahmedfurkhan@users.noreply.github.com>
2025-02-07 00:00:38 +0530

    Time: 267 ms (98.58%), Space: 46.2 MB (78.2%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.py
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.py
@@ -1,22 +1,16 @@
-from typing import List
-
 class Solution:
-    def tupleSameProduct(self, nums: List[int]) -> int:
-        product_map = dict()  # To store the frequency of each product
-        res = 0  # To store the final result
+  def tupleSameProduct(self, nums: List[int]) -> int:
+    n, result = len(nums), 0
+    product_freq = {}
 
-        # Step 1: Generate all possible products of pairs (a, b)
-        for idx, num in enumerate(nums[:-1]):
-            for numb in nums[idx + 1:]:
-                product = num * numb
-                if product in product_map:
-                    product_map[product] += 1
-                else:
-                    product_map[product] = 1
+    for i in range(n):
+      for j in range(i+1, n):  
+        product = nums[i] * nums[j]
+        if product in product_freq:
+          result += 8 * product_freq[product]
+          product_freq[product] += 1
+        else:
+          product_freq[product] = 1
 
-        # Step 2: Count valid tuples
-        for v in product_map.values():
-            if v > 1:
-                res += (v * (v - 1) // 2) * 8  # Correct formula
-
-        return res
+    return result
+        
