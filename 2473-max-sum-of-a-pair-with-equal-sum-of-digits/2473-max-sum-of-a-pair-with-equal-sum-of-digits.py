21c0ef90e203ff5cccaec3c8cf9815573b3f86d4
Ahmed Furkhan <108826853+Ahmedfurkhan@users.noreply.github.com>
2025-02-12 23:36:43 +0530

    Time: 251 ms (96.7%), Space: 32.9 MB (90.42%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -1,10 +1,30 @@
+from collections import defaultdict
+
 class Solution:
     def maximumSum(self, nums: List[int]) -> int:
-        max_arr = [0] * 82
-        ans = -1
-        for x in nums:
-            digit_sum = sum(int(d) for d in str(x))
-            if max_arr[digit_sum] != 0:
-                ans = max(ans, x + max_arr[digit_sum])
-            max_arr[digit_sum] = max(max_arr[digit_sum], x)
-        return ans
+        # We can group on dictionary, where key is sum of the digits of each num[i]
+        # and value is the array containing all the nums[j] that have the same sum of digits
+        # Later on iterate over arrays and get max value.
+        
+        # Define an inner function
+        def sum_num_digits(num: int)->int:
+            digit_sum = 0
+            while num:
+                digit_sum += num % 10
+                num //= 10
+            return digit_sum
+
+        num_grouping = defaultdict(list)
+        for num in nums:
+            digits_sum = sum_num_digits(num)
+            num_grouping[digits_sum].append(num)
+        answer = -1
+        for values in num_grouping.values():
+            if len(values) > 1:
+                values.sort(reverse=True)
+                # We need the biggest sum of num[i] + num[j] so sort in descending order
+                answer = max(answer, values[0]+values[1])
+        return answer    
+
+            
+        
